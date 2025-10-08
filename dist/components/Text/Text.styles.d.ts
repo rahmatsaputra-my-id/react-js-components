ff30394c4c173e49a497a20b0da55b51d22e1651
rahmatsaputra-my-id <rahmatsaputra.my.id@gmail.com>
2025-10-08 12:15:34 +0700

    - update v0.0.88

--- dist/components/Text/Text.styles.d.ts
+++ dist/components/Text/Text.styles.d.ts
@@ -1,116 +1,91 @@
 export declare const styles: {
     h1: {
         fontSize: number;
-        lineHeight: number;
     };
     h2: {
         fontSize: number;
-        lineHeight: number;
     };
     h3: {
         fontSize: number;
-        lineHeight: number;
     };
     h4: {
         fontSize: number;
-        lineHeight: number;
     };
     h5: {
         fontSize: number;
-        lineHeight: number;
     };
     h6: {
         fontSize: number;
-        lineHeight: number;
     };
     normal_10: {
         fontSize: number;
-        lineHeight: number;
     };
     normal_10_grey: {
         color: "#B0B0B0" | undefined;
         fontSize: number;
-        lineHeight: number;
     };
     bold_10: {
         fontSize: number;
         fontWeight: string;
-        lineHeight: number;
     };
     normal_12: {
         fontSize: number;
-        lineHeight: number;
     };
     normal_12_blue: {
         color: "#227AEA" | undefined;
         fontSize: number;
-        lineHeight: number;
     };
     normal_12_grey: {
         color: "#B0B0B0" | undefined;
         fontSize: number;
-        lineHeight: number;
     };
     bold_12: {
         fontSize: number;
         fontWeight: string;
-        lineHeight: number;
     };
     normal_14: {
         fontSize: number;
-        lineHeight: number;
     };
     bold_14: {
         fontSize: number;
         fontWeight: string;
-        lineHeight: number;
     };
     normal_16: {
         fontSize: number;
-        lineHeight: number;
     };
     bold_16: {
         fontSize: number;
         fontWeight: string;
-        lineHeight: number;
     };
     normal_18: {
         fontSize: number;
-        lineHeight: number;
     };
     bold_18: {
         fontSize: number;
         fontWeight: string;
-        lineHeight: number;
     };
     normal_20: {
         fontSize: number;
-        lineHeight: number;
     };
     bold_20: {
         fontSize: number;
         fontWeight: string;
-        lineHeight: number;
     };
     bold_20_white: {
         color: "#FFFFFF" | undefined;
         fontSize: number;
         fontWeight: string;
-        lineHeight: number;
     };
     normal_80: {
         fontSize: number;
-        lineHeight: number;
     };
     bold_80: {
         fontSize: number;
         fontWeight: string;
-        lineHeight: number;
     };
     bold_80_white: {
         color: "#FFFFFF" | undefined;
         fontSize: number;
         fontWeight: string;
-        lineHeight: number;
     };
 };
--- dist/index.es.js
+++ dist/index.es.js
@@ -257,117 +257,92 @@ var Container = function (_a) {
 var styles$6 = {
     h1: {
         fontSize: 32,
-        lineHeight: 40,
     },
     h2: {
         fontSize: 28,
-        lineHeight: 36,
     },
     h3: {
         fontSize: 24,
-        lineHeight: 32,
     },
     h4: {
         fontSize: 18,
-        lineHeight: 24,
     },
     h5: {
         fontSize: 16,
-        lineHeight: 24,
     },
     h6: {
         fontSize: 14,
-        lineHeight: 20,
     },
     normal_10: {
         fontSize: 10,
-        lineHeight: 12,
     },
     normal_10_grey: {
         color: Colors.grey5,
         fontSize: 10,
-        lineHeight: 12,
     },
     bold_10: {
         fontSize: 10,
         fontWeight: 'bold',
-        lineHeight: 12,
     },
     normal_12: {
         fontSize: 12,
-        lineHeight: 16,
     },
     normal_12_blue: {
         color: Colors.blue,
         fontSize: 12,
-        lineHeight: 16,
     },
     normal_12_grey: {
         color: Colors.grey5,
         fontSize: 12,
-        lineHeight: 16,
     },
     bold_12: {
         fontSize: 12,
         fontWeight: 'bold',
-        lineHeight: 16,
     },
     normal_14: {
         fontSize: 14,
-        lineHeight: 20,
     },
     bold_14: {
         fontSize: 14,
         fontWeight: 'bold',
-        lineHeight: 20,
     },
     normal_16: {
         fontSize: 16,
-        lineHeight: 24,
     },
     bold_16: {
         fontSize: 16,
         fontWeight: 'bold',
-        lineHeight: 24,
     },
     normal_18: {
         fontSize: 18,
-        lineHeight: 26,
     },
     bold_18: {
         fontSize: 18,
         fontWeight: 'bold',
-        lineHeight: 26,
     },
     normal_20: {
         fontSize: 20,
-        lineHeight: 28,
     },
     bold_20: {
         fontSize: 20,
         fontWeight: 'bold',
-        lineHeight: 28,
     },
     bold_20_white: {
         color: Colors.white,
         fontSize: 20,
         fontWeight: 'bold',
-        lineHeight: 28,
     },
     normal_80: {
         fontSize: 80,
-        lineHeight: 88,
     },
     bold_80: {
         fontSize: 80,
         fontWeight: 'bold',
-        lineHeight: 88,
     },
     bold_80_white: {
         color: Colors.white,
         fontSize: 80,
         fontWeight: 'bold',
-        lineHeight: 88,
     },
 };
 
