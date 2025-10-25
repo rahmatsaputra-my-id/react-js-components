971816dc5ed214d7bbf5d8fadc0aebc0fd75232f
rahmatsaputra-my-id <rahmatsaputra.my.id@gmail.com>
2025-10-25 12:42:25 +0700

    - update v0.0.174

--- dist/constants/Images/Images.types.d.ts
+++ dist/constants/Images/Images.types.d.ts
@@ -1,21 +1,22 @@
 export interface IIconOption {
+    arrow_right_thin?: string;
+    arrow_right?: string;
     camera?: string;
     close?: string;
     edit?: string;
-    rotate?: string;
-    send?: string;
-    image_not_available: string;
-    scan_qr?: string;
-    more?: string;
-    flash_on?: string;
-    flash_off?: string;
     exit?: string;
-    arrow_right?: string;
-    arrow_right_thin?: string;
+    flash_off?: string;
+    flash_on?: string;
+    image_not_available: string;
     information?: string;
-    question?: string;
     langugage?: string;
+    more?: string;
     printer?: string;
+    question?: string;
+    rotate?: string;
+    scan_qr?: string;
+    send?: string;
+    support?: string;
     tax?: string;
     terms_and_conditions?: string;
 }
